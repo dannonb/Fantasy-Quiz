42610259b3c1db26ffeeef1e28cab73aea0aadbe
dannonb <dannonbigham@gmail.com>
2020-07-17 05:26:22 -0700

    adding start script

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
-const socketio = require('socket.io')
+
 
 
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
-const io = socketio(server)
+
 
 const pubDir = path.join(__dirname, '../pub')
 
